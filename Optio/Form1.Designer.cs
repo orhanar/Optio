73f7b596ce028e8517ce025684d859e3f344a349
orhana <lemikistu@gmail.com>
2023-05-30 14:52:57 +0100

    Remove ignored files

--- Optio/Form1.Designer.cs
+++ Optio/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Optio
 {
-    partial class Form1
+    partial class Count
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            txtStartFrom = new TextBox();
+            txtTo = new TextBox();
+            btnCount = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(54, 122);
+            label1.Margin = new Padding(6, 0, 6, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(180, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Start From";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(427, 122);
+            label2.Margin = new Padding(6, 0, 6, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 45);
+            label2.TabIndex = 1;
+            label2.Text = "To";
+            // 
+            // txtStartFrom
+            // 
+            txtStartFrom.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtStartFrom.Location = new Point(230, 117);
+            txtStartFrom.Margin = new Padding(6, 6, 6, 6);
+            txtStartFrom.Name = "txtStartFrom";
+            txtStartFrom.Size = new Size(182, 50);
+            txtStartFrom.TabIndex = 2;
+            txtStartFrom.TextChanged += textBox1_TextChanged;
+            // 
+            // txtTo
+            // 
+            txtTo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtTo.Location = new Point(490, 117);
+            txtTo.Margin = new Padding(6, 6, 6, 6);
+            txtTo.Name = "txtTo";
+            txtTo.Size = new Size(182, 50);
+            txtTo.TabIndex = 3;
+            // 
+            // btnCount
+            // 
+            btnCount.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCount.Location = new Point(249, 215);
+            btnCount.Margin = new Padding(6, 6, 6, 6);
+            btnCount.Name = "btnCount";
+            btnCount.Size = new Size(163, 68);
+            btnCount.TabIndex = 4;
+            btnCount.Text = "Count";
+            btnCount.UseVisualStyleBackColor = true;
+            btnCount.Click += btnCount_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnExit.Location = new Point(490, 215);
+            btnExit.Margin = new Padding(6, 6, 6, 6);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(163, 68);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // Count
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1372, 960);
+            Controls.Add(btnExit);
+            Controls.Add(btnCount);
+            Controls.Add(txtTo);
+            Controls.Add(txtStartFrom);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "Count";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Count";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtStartFrom;
+        private TextBox txtTo;
+        private Button btnCount;
+        private Button btnExit;
     }
 }
--- Optio/Form1.cs
+++ Optio/Form1.cs
@@ -1,10 +1,39 @@
 namespace Optio
 {
-    public partial class Form1 : Form
+    public partial class Count : Form
     {
-        public Form1()
+        public Count()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int firstNumber, lastNumber = 0;
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToInt32(txtStartFrom.Text);
+            lastNumber = Convert.ToInt32(txtTo.Text);
+            MessageBox.Show("From " + firstNumber + " To " + lastNumber);
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
